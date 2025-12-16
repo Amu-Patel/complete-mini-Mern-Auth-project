3e1adb88a32a80c09cd455ead7d406a85a813efe
AMISHA PATEL <147070843+Amu-Patel@users.noreply.github.com>
2025-12-17 05:02:03 +0530

    Update frontend login

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import {createContext} from "react";
+
+export const AppContent = createContext()
+
+export const AppContextProvider = (props)=>{
+
+    const value={
+
+    }
+
+    return(
+        <AppContent.Provider value={value}>
+            {props.children}
+        </AppContent.Provider>
+    )
+
+
+
+
+}
